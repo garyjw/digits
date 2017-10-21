7516df462a490aea61f44ad186b09cc3d187f057
garyjw <31455599+garyjw@users.noreply.github.com>
2017-10-20 22:00:18 -1000

    digits 4

--- app/imports/startup/server/index.js
+++ app/imports/startup/server/index.js
@@ -1,3 +1,5 @@
 import './accounts.js';
 import './stuff.js';
 import './contacts.js';
+import './publications.js';
+
---
+++ app/imports/ui/components/form-controls/radio-field.js
@@ -0,0 +1,6 @@
+
+import { Template } from 'meteor/templating';
+
+Template.Radio_Field.onRendered(function onRendered() {
+  this.$('.ui.radio.checkbox').checkbox();
+});
